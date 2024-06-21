40cc84e8288903558d455d1aa23a02370f21bd4f
Thales Maia <thalesmaiaalexandre@gmail.com>
2024-06-21 13:15:52 -0300

    feat: Registering notFoundException on global Exception handler

---
+++ src/main/java/org/example/codechallenge/exceptions/ExceptionMessage.java
@@ -0,0 +1,33 @@
+package org.example.codechallenge.exceptions;
+
+
+
+public class ExceptionMessage {
+
+    private int status;
+    private String message;
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public ExceptionMessage(int status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+
+    public ExceptionMessage() {
+    }
+}
--- src/main/java/org/example/codechallenge/exceptions/ExceptionsHandler.java
+++ src/main/java/org/example/codechallenge/exceptions/ExceptionsHandler.java
@@ -6,12 +6,15 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @ControllerAdvice
 public class ExceptionsHandler  {
+
+    private static final int NOT_FOUND_VALUE = HttpStatus.NOT_FOUND.value();
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Object> handleValidationExceptions(MethodArgumentNotValidException exception) {
@@ -21,6 +24,14 @@ public class ExceptionsHandler  {
             String errorMessage = error.getDefaultMessage();
             errors.put(fieldName, errorMessage);
         });
+
         return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    private ResponseEntity<ExceptionMessage> notFoundException(NotFoundException exception) {
+        ExceptionMessage messages = new ExceptionMessage(NOT_FOUND_VALUE, exception.getMessage());
+        return ResponseEntity.status(NOT_FOUND_VALUE).body(messages);
     }
 }
